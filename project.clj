b39498898027665fc9d5eb05bb3d256739da99f7
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-06-24 17:57:28 +0800

    fix

--- project.clj
+++ project.clj
@@ -5,7 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.seleniumhq.selenium/selenium-java "2.47.0"]
-                 [clj-webdriver "0.7.2"]]
+                 [org.seleniumhq.selenium/selenium-server "2.47.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.0"]
+                 [clj-webdriver "0.7.2" :exclusion [com.github.detro.ghostdriver/phantomjsdriver
+                                                    org.seleniumhq.selenium/selenium-server
+                                                    org.seleniumhq.selenium/selenium-java
+                                                    org.seleniumhq.selenium/selenium-remote-driver]]]
   :plugins [[cider/cider-nrepl "0.12.0"]]
   :main ^:skip-aot rocks.pho.weixin-robot.core
   :source-paths ["src/"]
