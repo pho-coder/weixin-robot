ed34c8fdc30475053aeb2f077ab53f6da289e153
Phoenix Zhang <pho.coderzhang@gmail.com>
2016-06-27 18:05:30 +0800

    fix

--- project.clj
+++ project.clj
@@ -4,13 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.seleniumhq.selenium/selenium-java "2.47.0"]
-                 [org.seleniumhq.selenium/selenium-server "2.47.0"]
-                 [org.seleniumhq.selenium/selenium-remote-driver "2.47.0"]
-                 [clj-webdriver "0.7.2" :exclusion [com.github.detro.ghostdriver/phantomjsdriver
-                                                    org.seleniumhq.selenium/selenium-server
-                                                    org.seleniumhq.selenium/selenium-java
-                                                    org.seleniumhq.selenium/selenium-remote-driver]]]
+                 [org.seleniumhq.selenium/selenium-java "2.53.0"]
+                 [org.seleniumhq.selenium/selenium-server "2.53.0"]
+                 [org.seleniumhq.selenium/selenium-chrome-driver "2.53.0"]
+                 [org.seleniumhq.selenium/selenium-remote-driver "2.53.0"]]
   :plugins [[cider/cider-nrepl "0.12.0"]]
   :main ^:skip-aot rocks.pho.weixin-robot.core
   :source-paths ["src/"]
--- src/rocks/pho/weixin_robot/utils.clj
+++ src/rocks/pho/weixin_robot/utils.clj
@@ -1,10 +1,23 @@
 (ns rocks.pho.weixin-robot.utils
-  (:require [clj-webdriver.taxi :as taxi]))
+  (:import [org.openqa.selenium WebDriver]
+           [org.openqa.selenium.chrome ChromeDriverService ChromeDriverService$Builder]
+           [org.openqa.selenium.remote DesiredCapabilities RemoteWebDriver]))
+
+(def ^:dynamic *driver* (atom nil))
 
 (defn get-a-browser
   []
-  (taxi/set-driver! {:browser :firefox}))
+  (let [service (.build (.usingAnyFreePort (.usingDriverExecutable (ChromeDriverService$Builder.)
+                                                                   (java.io.File. "/home/phoenix/bin/chromedriver"))))]
+    (.start service)
+    (let [capabilities (DesiredCapabilities/firefox)]
+      (reset! *driver* (RemoteWebDriver. (.getUrl service)
+                                         capabilities)))))
 
 (defn go-to
   []
-  (taxi/to "https://wx.qq.com"))
+  (.get @*driver* "https://wx.qq.com"))
+
+(defn get-screenshot
+  []
+  (.getScreenshotAs ))
